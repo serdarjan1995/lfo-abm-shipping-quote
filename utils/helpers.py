8db14467580b8af6e5d334732df804a45db43c52
Sardor <serdarjan1995@gmail.com>
2024-06-24 20:18:17 +0300

    fix N/A

--- utils/helpers.py
+++ utils/helpers.py
@@ -11,6 +11,8 @@ def try_parse_float(value: str):
     value = value.strip()
     try:
         value_stripped = value.replace("$", '').strip()
+        if value_stripped == "#N/A":
+            return 0
         return float(value_stripped)
     except (Exception, ValueError):
         return value
